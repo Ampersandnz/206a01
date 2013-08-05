ceea8bc9842f8275486031ec494a9fc9d2fec184
Michael Lo <nz.ampersand@gmail.com>
2013-08-05 20:52:33 +1200

    Completed DataTask Java, need to debug.

--- files/DataTask.java
+++ files/DataTask.java
@@ -15,16 +15,17 @@
  * 		If a data.txt already exists, it is over written.
  * 		
  * Usage:
- *  	./DataTask
+ *  	java DataTask >./data.txt
  */
 
 import java.nio.Path;
-import java.nio.file.*
+import java.nio.file.*;
+import java.io.*;
 
 public class DataTask {
 	public static void main (String[] args) {
 		private static int MAX = 999;
-		private static char DATA_FILE_NAME = "data.txt";
+		private static String DATA_FILE_NAME = "data.txt";
 
 		/* Delete the data file */
 		Path path = FileSystems.getDefault().getPath(DATA_FILE_NAME);
@@ -41,63 +42,57 @@ public class DataTask {
 
 		/*Get array of all files in directory*/
 		File[] contents = directory.listFiles();
+
+		List<String> strings = new ArrayList<String>();
+		String currentData;
 		/* Loop through all files in directory */
 		for ( File f : contents) {
-			String currentFileName = f.getname();
+			currentData = getData(f);
+			if (!strings.contains(currentData)) {
+				strings.add(currentData);
+			}
+		}
 
-			/* Filter out files that do not contain "contact" in its file name */
-			if (currentFileName.toLowerCase().contains("contact")) {
-				
+		Collections.sort(strings); 
+		output(strings, DATA_FILE_NAME);
+	}
+
+	private String getData(File f) {
+		String currentFileName = f.getname();
+
+		/* Filter out files that do not contain "contact" in its file name */
+		if (currentFileName.toLowerCase().contains("contact")) {
+			StringBuffer contents = new StringBuffer();
+			BufferedReader reader = null;
+			try {
+				reader = new BufferedReader(new FileReader(f));
+				return reader.readLine();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (reader != null) {
+						reader.close();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
-}
 
-		/* Open the file and get its content */
-		char fileContent[MAX];		
-		FILE *filePointer = fopen(fileEntry->d_name, "r");
-		fgets(fileContent, MAX, filePointer);
-		fclose(filePointer);
-		
-		/* Loop through existing contents stored in filesContent, checking for duplicates with current content */
-		int i;
-		int foundDuplicate = 0;
-		for(i=0; i<filesContentCount; i++)
-		{
-			if(strcmp(fileContent, filesContent[i]) == 0)
-			{
-				foundDuplicate=1;
-				break;
-			}
+	private Static output (String [] strings, String DATA_FILE_NAME) {
+		Path path = FileSystems.getDefault().getPath(DATA_FILE_NAME);
+		OutputStream out = Files.newOutputStream(path, CREATE, APPEND);
+		String newLine = System.getProperty("line.separator");
+
+		/*For each string in strings, append it to the file, followed by a new line.*/
+		for (String s: strings) {
+			out.write(s, 0, s.length);
+			out.write(newLine, 0, newLine.length);
 		}
-		if (foundDuplicate == 1)
-		{
-			continue;
-		}
-		
-		/* Add the data to the array if it passed all above checks */
-		filesContent[filesContentCount] = malloc(strlen(fileContent)+1);
-		strcpy(filesContent[filesContentCount], fileContent);
-		filesContentCount++;
+		out.close();
 	}
-	closedir(directoryPointer);
-
-	/* Sort the filesContent array. (Hint: Equivalent to the bash sort command) */
-	qsort(filesContent, filesContentCount, sizeof(*filesContent), comparator);
-	
-	/* Write the each file content as to the array separated by new lines */
-	FILE *filePointer = fopen(DATA_FILE_NAME, "w");	
-	int i;
-	for(i=0; i<filesContentCount; i++)
-	{
-		fprintf(filePointer, "%s", filesContent[i]);
-	}
-	fclose(filePointer);
-}
-
-/* Used for comparing strings when sorting */
-/* (You do not need to fully understand this function to complete the assignment) */
-int comparator(const void *left, const void *right)
-{
-	return (strcmp(*(char **)left, *(char **)right));
 }
