a28ba217e3483748dea02d2a68a2d10d5271b1b7
Michael Lo <nz.ampersand@gmail.com>
2013-08-09 17:40:24 +1245

    FileTask.sh works completely, still need to check and complete both Java tasks and DataTask.sh

--- files/FileTask.sh
+++ files/FileTask.sh
@@ -9,7 +9,7 @@
  # Description:
  # 		Creates a .contact file.
  # Usage:
- #		./FileTask [DIRECTORY] [CONTACT_NAME] [CONTACT_NUMBER]
+ #		./FileTask.sh [DIRECTORY] [CONTACT_NAME] [CONTACT_NUMBER]
  # Example: 
  #		./FileTask . john 123456
 ##
@@ -22,47 +22,52 @@ if [ $# -le 2 ]
 then
 	# === Using standard input === #	
 	echo "Directory: "
-	directory = read year
+	read dir
+	directory=$dir
 	echo "Contact Name: "
-	contactName = read year	
+	read name
+	contactName=$name
 	echo "Contact Number: "
-	contactNumber = read year
+	read number
+	contactNumber=$number
 else
 	# == Using command line arguments === #
-	while getopts d:c:n: flag; 
-	do
-		case $flag in
-		d)
-			directory = $OPTARG;;
-		c)
-			contactName = $OPTARG;;
-		n)
-			contactNumber = $OPTARG;;
-		?)
-			exit;;
-		esac
-	done
+	directory=$1
+	contactName=$2
+	contactNumber=$3
 fi
+
+echo $directory
+echo $contactName
+echo $contactNumber
 
 	# ============ #
 	# Main Program #
 	# ============ #
 	
 # The file path is in the format of <path to file>/<contact name>.contact #
-filePath = "/$directory/$contactName.contact";
+filePath="./$directory/$contactName.contact";
 	
 if [ ! -e $filePath ]; then
 	# === File (or contact) does not exist, so create one and write in correct phone number === #
-	echo $contactNumber > $filePath
+	mkdir -p $directory
+	touch $filePath || exit
+	echo $contactNumber>$filePath
 else	
 	# === File (or contact) already exists === #
-	i = 1;
+	i=1
 	# Loop through for example john(1).contact, john(2).contact, john(3).contact and so on... #
 	# until one of them does not exist, then we write the contact number to that file #
 	for (( ; ; ))
 	do
-		filePath = "/$directory/$contactName($i).contact"
-   		echo $contactNumber > $filePath
-   	i = $i + 1
+		if [ ! -e $filePath ]; then
+			mkdir -p $directory
+			touch $filePath || exit
+   			echo $contactNumber>$filePath
+			break
+		else
+			filePath="./$directory/$contactName($i).contact"
+		fi
+   	i=$[i + 1]
 	done
 fi
